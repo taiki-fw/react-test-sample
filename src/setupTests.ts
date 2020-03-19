1e6e94d3a403a090346978aaec956450d23696a9
taiki-fw <taiki_52@outlook.jp>
2020-03-19 19:47:19 +0900

    jestにてtypescriptで書かれたテストファイルを実行する上で必要なモジュールの導入

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,4 +2,12 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom/extend-expect';
+import "@testing-library/jest-dom/extend-expect";
+
+import { GlobalWithFetchMock } from "jest-fetch-mock";
+
+const customGlobal: GlobalWithFetchMock = global as GlobalWithFetchMock;
+customGlobal.fetch = require("jest-fetch-mock");
+customGlobal.fetchMock = customGlobal.fetch;
+
+export default customGlobal;
