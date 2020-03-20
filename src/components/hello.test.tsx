95122845bd514e7ad29058609279971f8947725f
taiki-fw <taiki_52@outlook.jp>
2020-03-20 16:04:39 +0900

    helloコンポーネントにスナップショットを追加

--- src/components/hello.test.tsx
+++ src/components/hello.test.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { render, unmountComponentAtNode } from "react-dom";
 import { act } from "react-dom/test-utils";
+import shaping from "pretty";
 
 import Hello from "./hello";
 
@@ -20,15 +21,24 @@ it("renders with or without name", () => {
     render(<Hello />, container);
   });
   expect(container.textContent).toBe("Hey, stranger");
+  expect(shaping(container.innerHTML)).toMatchInlineSnapshot(
+    `"<span>Hey, stranger</span>"`
+  );
 
   act(() => {
     // containerに別のコンポーネントをレンダーしている。
     render(<Hello name="Jenny" />, container);
   });
   expect(container.textContent).toBe("Hello, Jenny!");
+  expect(shaping(container.innerHTML)).toMatchInlineSnapshot(
+    `"<h1>Hello, Jenny!</h1>"`
+  );
 
   act(() => {
     render(<Hello name="Margaret" />, container);
   });
   expect(container.textContent).toBe("Hello, Margaret!");
+  expect(shaping(container.innerHTML)).toMatchInlineSnapshot(
+    `"<h1>Hello, Margaret!</h1>"`
+  );
 });
